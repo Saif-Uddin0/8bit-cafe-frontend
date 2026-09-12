858037a07bf866dbcefb37dc6e30b2bed8526124
Saif Uddin <mahirr.846@gmail.com>
2026-09-12 15:06:19 +0600

    fixed the signup page

--- src/app/(authentication_home)/signup/page.tsx
+++ src/app/(authentication_home)/signup/page.tsx
@@ -405,9 +405,9 @@ export default function SignUpPage() {
             </span>
             <div className="flex-1 h-px bg-purple-400/25" />
           </div>
-
+{/* 
           ── Social Buttons ──
-          {/* <div className="flex justify-center gap-4">
+          <div className="flex justify-center gap-4">
             <button
               type="button"
               className="flex items-center justify-center gap-2.5 px-8 py-3 sm:py-3.5 rounded-2xl bg-[#181426]/60 border border-white text-white text-xs sm:text-sm font-medium transition-all duration-200 hover:border-white/40 hover:bg-[#1c1636]/85 cursor-pointer"
